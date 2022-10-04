6a457280ca8d30fa5fb9843f231397a61d7bed6a
Raoni Andrade <raonio.andrade@gmail.com>
2022-10-04 18:30:17 -0300

    05 done

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,6 +2,25 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      artistName: '',
+    };
+  }
+
+  enableButton = () => {
+    const { artistName } = this.state;
+    const min = 2;
+    return artistName.length >= min;
+  };
+
+  handleArtist = (event) => {
+    this.setState({
+      artistName: event.target.value,
+    });
+  };
+
   render() {
     return (
       <div data-testid="page-search">
@@ -9,6 +28,25 @@ class Search extends React.Component {
         <h1>
           Search
         </h1>
+        <form>
+          <label htmlFor="artist">
+            <input
+              placeholder="Nome do Artista"
+              id="artist"
+              data-testid="search-artist-input"
+              type="text"
+              onChange={ this.handleArtist }
+            />
+          </label>
+          <button
+            type="button"
+            disabled={ !this.enableButton() }
+            data-testid="search-artist-button"
+            // onClick={}
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
